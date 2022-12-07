5930f2def5f8dd42cc033168b0e992b0b0b221b7
helin <lin.he@sysbox.com.au>
2022-12-08 10:28:25 +1100

    master: upgraded.

--- src/pages/SetupPage.jsx
+++ src/pages/SetupPage.jsx
@@ -11,8 +11,11 @@ const SetupPage = ({notification}) => {
 
   return (
     <Fragment>
-      <SectionMessage title="Purpose of this app" appearance="info">
-        <Text>Designed to play a sound when an issue is created (updated/deleted is WIP...)</Text>
+      <Text>Designed to play a sound when an issue is created (updated/deleted is WIP...)</Text>
+      <SectionMessage title="Critical: Change of address" appearance="warning">
+        <Text>As our service provider cutting off our free service, we have to move to another server</Text>
+        <Text>PLEASE UPDATE YOUR JIRA AUTOMATION TO THE NEW ADDRESS IN STEP 1</Text>
+        <Text>to avoid any disruption of services</Text>
       </SectionMessage>
       <SetupStepOne notification={notification} />
       <SetupStepTwo notification={notification} />
